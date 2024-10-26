88f23245cf96fddadaa3f3bca353182b49e22ee4
Saugat Guni <saugat.guni@gmail.com>
2024-10-26 12:29:09 -0400

    fixed tf errors

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+
+import tensorflow.keras
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
